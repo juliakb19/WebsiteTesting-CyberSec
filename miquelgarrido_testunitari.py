d4d02dbcb845ec725775c7d38d60bced77dbc7c1
MiquelGarrido <94826123+MiquelGarrido@users.noreply.github.com>
2024-10-17 20:32:50 +0200

    Update miquelgarrido_testunitari.py

--- miquelgarrido_testunitari.py
+++ miquelgarrido_testunitari.py
@@ -1,28 +1,32 @@
+import pytest
+import unittest
+import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Inicializar el navegador (por ejemplo, Chrome)
-driver = webdriver.Chrome()
+def test_garrido():
+    driver = webdriver.Chrome()
 
-# Abrir la página web
-driver.get("http://www.cibergrup1.cecti.iesmontsia.cat")
 
-try:
-    # Esperar hasta que el meta tag esté presente (hasta 10 segundos)
-    meta_description = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//meta[@name='description']"))
-    )
-    
-    # Obtener el valor del atributo content
-    description_content = meta_description.get_attribute("content")
+    driver.get("http://www.cibergrup1.cecti.iesmontsia.cat")
 
-    # Mostrar el valor obtenido
-    print(f"Meta description content: {description_content}")
-    
-except Exception as e:
-    print(f"Error: {e}")
+    try:
+        
+        meta_description = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//meta[@name='description']"))
+        )
+        
+        description_content = meta_description.get_attribute("content")
+        
+        assert description_content is not None and description_content != "", "Error: Meta description no trobada o buida"
+        print(f"Meta description content: {description_content}")
+        
+        #print(f"Meta description content: {description_content}")
+        
+    except Exception as e:
+        print(f"Error: {e}")
 
-# Cerrar el navegador
-driver.quit()
+
+    driver.quit()
